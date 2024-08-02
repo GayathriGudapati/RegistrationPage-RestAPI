44d9e5c8d415a52fb1cb1ee5a3637dd5b812a062
BALA GAYATHRI GUDAPATI <152675023+GayathriGudapati@users.noreply.github.com>
2024-08-02 23:39:18 +0530

    REST API

--- demo22/src/main/java/com/example/demo/controller/registrationcontroller.java
+++ demo22/src/main/java/com/example/demo/controller/registrationcontroller.java
@@ -1,6 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +25,24 @@ public class registrationcontroller {
 	public registration addinguser(@RequestBody registration reg) {
 		return service.addinguser(reg);
 	}
-//	
-//	public list<registration> multifetch(){
-//		
-//	}
+	@PostMapping("/addall")
+	public Iterable<registration> addall(@RequestBody List<registration> reg) {
+		return service.addall(reg);
+	}
+	
+	@GetMapping("/fetchall")
+	public List<registration> fetchall() {
+		return service.fetchall();
+	}
+	
+	@GetMapping("/fetchbyid/{id}")
+	public Optional<registration> fetchbyid(@PathVariable int id) {
+		return service.fetchbyid(id);
+	}
+	@DeleteMapping("/delbyid/{id}")
+	public String delbyid(@PathVariable int id) {
+		return service.delbyid(id);
+	}
 	@PutMapping("/update")
 	public registration updating(@RequestBody registration reg) {
 		return service.update(reg);
--- demo22/src/main/java/com/example/demo/services/registrationservice.java
+++ demo22/src/main/java/com/example/demo/services/registrationservice.java
@@ -1,5 +1,9 @@
 package com.example.demo.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,22 @@ public class registrationservice {
 	public registration addinguser(registration reg) {
 		return repo.save(reg);
 	}
+	public Iterable<registration> addall(List<registration> reg) {
+		return repo.saveAll(reg);
+	}
+	
+	public List<registration> fetchall(){
+		return (List<registration>) repo.findAll();
+	}
+	
+	public Optional<registration> fetchbyid(int id) {
+		return repo.findById(id);
+	}
+	
+	public String delbyid(int id) {
+		repo.deleteById(id);
+		return "successfully deleted : "+id;
+	}
 	public registration update(registration reg) {
 		int id=reg.getId();
 		registration reg1=repo.findById(id).get();
@@ -23,4 +43,5 @@ public class registrationservice {
 		reg1.setGender(reg.getGender());
 		return repo.save(reg1);
 		}
+	
 }
